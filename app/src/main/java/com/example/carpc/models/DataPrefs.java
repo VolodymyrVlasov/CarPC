17c81ec28b1ab59afebd608a41b611fc91fcc97a
VolodymyrVlasov <volodymyr.vlasov@gmail.com>
2020-12-27 18:14:07 +0200

    add counter

--- app/src/main/java/com/example/carpc/models/DataPrefs.java
+++ app/src/main/java/com/example/carpc/models/DataPrefs.java
@@ -2,6 +2,7 @@ package com.example.carpc.models;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+
 import com.example.carpc.utils.AppConstants;
 
 import java.util.Map;
@@ -27,11 +28,11 @@ public class DataPrefs {
     }
 
     public String getIP() {
-        return sharedPref.getString(DATA_IP, "127.0.0.1");
+        return sharedPref.getString(DATA_IP, "192.168.1.7");
     }
 
     public void setIP(String IP) {
-        sharedPref.edit().putString(AppConstants.DATA_IP, IP);
+        sharedPref.edit().putString(AppConstants.DATA_IP, IP).apply();
     }
 
     public int getPort() {
@@ -39,10 +40,44 @@ public class DataPrefs {
     }
 
     public void setPort(int port) {
-        sharedPref.edit().putInt(AppConstants.DATA_PORT, port);
+        sharedPref.edit().putInt(AppConstants.DATA_PORT, port).apply();
     }
 
     public Map<String, ?> GetAll() {
         return sharedPref.getAll();
+    }
+
+    public double getUsedAmpereHour() {
+        int temp = sharedPref.getInt(AppConstants.DATA_BATTERY_USED_AH, 0);
+        return temp != 0 ? (double) (temp / 10) : 0;
+    }
+
+    public double getUsedWattHour() {
+        int temp = sharedPref.getInt(AppConstants.DATA_BATTERY_USED_WH, 0);
+        return temp != 0 ? (double) (temp / 10) : 0;
+    }
+
+    public double getTotalAmpereHour() {
+        int temp = sharedPref.getInt(AppConstants.DATA_BATTERY_CAPACITY_AH, 0);
+        return temp != 0 ? (double) (temp / 10) : 0;
+    }
+
+    public double getTotalWattHour() {
+        int temp = sharedPref.getInt(AppConstants.DATA_BATTERY_CAPACITY_WH, 0);
+        return temp != 0 ? (double) (temp / 10) : 0;
+    }
+
+    public void setUsedAmpereHour(Double ampereHour) {
+        sharedPref.edit().putInt(AppConstants.DATA_BATTERY_USED_AH, (int) (ampereHour * 10)).apply();
+
+    }
+
+    public void setUsedWattHour(Double ampereHour) {
+        sharedPref.edit().putInt(AppConstants.DATA_BATTERY_USED_WH, (int) (ampereHour * 10)).apply();
+    }
+
+    public void commit() {
+        sharedPref.edit().apply();
+
     }
 }
--- app/src/main/java/com/example/carpc/utils/Counter.java
+++ app/src/main/java/com/example/carpc/utils/Counter.java
@@ -1,11 +1,52 @@
 package com.example.carpc.utils;
 
-public class Counter {
+import com.example.carpc.MainActivity;
 
-    //TODO Create methods for:
-    // - return ampere hour
-    // - return watt hour
-    // - return trip distance (current trip view)
-    // - return average consumption
-    // - return average speed
+import java.util.Timer;
+import java.util.TimerTask;
+
+//TODO Create methods for:
+// - return ampere hour
+// - return watt hour
+// - return trip distance (current trip view)
+// - return average consumption
+// - return average speed
+
+public class Counter {
+    private static DataParser parser;
+    private static final long DELAY = 10L;
+    private static final long PERIOD = 10L;
+    private static double ampereHour;
+    private static double wattHour;
+
+    public Counter() {
+        parser = (DataParser) MainActivity.getParser();
+        update();
+    }
+
+    public static double getUsedAH() {
+        return ampereHour;
+    }
+
+    public static double getUsedWH() {
+        return wattHour;
+    }
+
+    public static void setUsedAH(double ampereHour) {
+        Counter.ampereHour = ampereHour;
+    }
+
+    public static void setUsedWH(double wattHour) {
+        Counter.wattHour = wattHour;
+    }
+
+    public void update() {
+        TimerTask repeatedTask = new TimerTask() {
+            public void run() {
+                ampereHour = ampereHour + (parser.getCurrent() / 3600) / 100;
+                wattHour = wattHour + (parser.getCurrent() * parser.getVoltage() / 3600) / 100000;
+            }
+        };
+        new Timer().scheduleAtFixedRate(repeatedTask, DELAY, PERIOD);
+    }
 }
